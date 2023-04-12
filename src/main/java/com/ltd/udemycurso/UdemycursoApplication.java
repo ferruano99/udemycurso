8f72461905bd8595c4e6fd18e64030f5971dbfbf
Fernando <fernando.ruano.e@gmail.com>
2023-04-12 17:42:41 +0200

    First

---
+++ src/main/java/com/ltd/udemycurso/UdemycursoApplication.java
@@ -0,0 +1,13 @@
+package com.ltd.udemycurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemycursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemycursoApplication.class, args);
+	}
+
+}
